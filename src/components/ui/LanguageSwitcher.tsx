ff6fd90efdbb6bca12e1c626cf4a3afa668c1605
abdelbouda <abdelbouda@gmail.com>
2026-05-08 00:20:07 +0200

    Update LanguageSwitcher.tsx

--- src/components/ui/LanguageSwitcher.tsx
+++ src/components/ui/LanguageSwitcher.tsx
@@ -1,34 +1,13 @@
-import { useState } from 'react';
-import {
-  switchLocale,
-  getAlternativeLocale
-} from '../../i18n/utils';
-
 export default function LanguageSwitcher() {
-  const [open, setOpen] = useState(false);
-
   const currentLocale = Astro.currentLocale;
-  const alternativeLocale = getAlternativeLocale(currentLocale);
+  const alternativeLocale = currentLocale === 'en' ? 'nl' : 'en';
 
   return (
-    <div class="relative">
-      <button
-        class="text-gray-700 hover:text-[#001F3F]"
-        onClick={() => setOpen(!open)}
-      >
-        {currentLocale.toUpperCase()}
-      </button>
-
-      {open && (
-        <div class="absolute right-0 mt-2 bg-white border rounded shadow">
-          <a
-            href={switchLocale(alternativeLocale)}
-            class="block px-4 py-2 hover:bg-gray-100"
-          >
-            {alternativeLocale.toUpperCase()}
-          </a>
-        </div>
-      )}
-    </div>
+    <a
+      href={`/${alternativeLocale}`}
+      class="text-gray-700 hover:text-[#001F3F]"
+    >
+      {alternativeLocale.toUpperCase()}
+    </a>
   );
 }
